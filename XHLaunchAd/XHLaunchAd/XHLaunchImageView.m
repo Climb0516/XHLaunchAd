1fded1f8bbab3fe4ee3875a2eb5aca4d3d8ba283
Climb 王 <wangpandeng1223@613.com>
2020-03-03 14:31:28 +0800

    因微脉项目匿名接口token的独特启动逻辑使然，故launchScreen.Storyboard作为启动图，也必须保留launchLaunchViewController作为承接闪屏和匿名接口请求之间的时间差，所以开屏广告还是得作为独立一部分，流程不能进一步优化。 遂把XHLaunchAd开源代码Fork下来，然后进行源码修改，发布版本，使项目里使用修改过的发布版本；把LaunchScreenSb修改为全局变量，消除内存泄漏引起的偶发性闪退。

--- XHLaunchAd/XHLaunchAd/XHLaunchImageView.m
+++ XHLaunchAd/XHLaunchAd/XHLaunchImageView.m
@@ -11,6 +11,9 @@
 
 
 @interface XHLaunchImageView ()
+{
+    UIViewController *LaunchScreenSb;
+}
 
 @end
 
@@ -53,7 +56,7 @@
         XHLaunchAdLog(@"从 LaunchScreen 中获取启动图失败!");
         return nil;
     }
-    UIViewController *LaunchScreenSb = [[UIStoryboard storyboardWithName:UILaunchStoryboardName bundle:nil] instantiateInitialViewController];
+    LaunchScreenSb = [[UIStoryboard storyboardWithName:UILaunchStoryboardName bundle:nil] instantiateInitialViewController];
     if(LaunchScreenSb){
         UIView * view = LaunchScreenSb.view;
         // 加入到UIWindow后，LaunchScreenSb.view的safeAreaInsets在刘海屏机型才正常。
